f3225794b2fc494ee5b0f62fa26e454a8cb5605f
alejoboga20 <alejoboga19@gmail.com>
2024-12-18 16:55:20 -0300

    create product

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description, price } = createProductDto;
+
+    const newProduct = new Product(uuid(), name, description, price);
+    this.products.push(newProduct);
+
+    return newProduct;
   }
 
   findAll() {
